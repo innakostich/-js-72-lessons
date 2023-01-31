0c4bb910970558f3ca0882b0eee62c77a812920e
Inna Kostich <114675831+innakostich@users.noreply.github.com>
2023-01-31 11:07:47 +0100

    123

--- js/01-switch.js
+++ js/01-switch.js
@@ -101,27 +101,46 @@
  */
 
 //  1. сделать переменные
-const option = 1526;
-let message = '';
+//const option = 123;
+//let message = '';
 
 // 2. сделать switch 1 2 3
 // 3. в каждом кейсе записать в message строку
-switch (option) {
-  case 1:
-    message = 'Вы сможете забрать товар завтра с 12:00 в нашем офисе';
-    break;
+//switch (option) {
+ // case 1:
+ //   message = 'Вы сможете забрать товар завтра с 12:00 в нашем офисе';
+ //   break;
 
-  case 2:
-    message = 'Курьер доставит заказ завтра с 9:00 до 18:00';
-    break;
+ // case 2:
+  //  message = 'Курьер доставит заказ завтра с 9:00 до 18:00';
+  //  break;
 
-  case 3:
-    message = 'Посылка будет отправлена сегодня';
-    break;
+  //case 3:
+ //   message = 'Посылка будет отправлена сегодня';
+  //  break;
 
-  default:
-    message = 'Вам перезвонит менеджер';
-}
+ // default:
+  //  message = 'Вам перезвонит менеджер';
+//}
 
 // 4. сделать лог message
-console.log(message);
+//console.log(message);
+const string = 'JavaScript';
+
+const letters = string.split('');
+
+let invertedString = "";
+
+console.log (letters);
+
+for ( const letter of letters) {
+  console.log(letter)
+;
+
+ if (letter === letter.toLowerCase()) {
+  console.log ('This is small case .', letter);
+  invertedString += letter.toUpperCase();
+ }
+
+ console.log( invertedString);
+}
